af984ab0a1af607e66fbc1aae86eb42146d53e6c
Jonathan Gamble <jdgamble555@gmail.com>
2024-03-02 15:28:42 -0600

    test

--- api/index.cjs
+++
@@ -1,3 +0,0 @@
-const server = import('../dist/angular-vercel-ssr/server/server.mjs');
-
-module.exports = server.app;
---
+++ api/index.js
@@ -0,0 +1,3 @@
+import * as server from '../dist/angular-vercel-ssr/server/server.mjs';
+
+export default server.app;
