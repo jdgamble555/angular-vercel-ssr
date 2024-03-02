9a5f43fba954df7ae6cf6fb8a533a883db53b873
Jonathan Gamble <jdgamble555@gmail.com>
2024-03-02 16:04:57 -0600

    test

--- api/index.js
+++ api/index.js
@@ -1,3 +1,3 @@
-const server = await import('../dist/angular-vercel-ssr/server/server.mjs');
+const server = async () => await import('../dist/angular-vercel-ssr/server/server.mjs');
 
 module.exports = server.app();
